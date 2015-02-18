b00ab85415ff7bc7faa1b6aa05abb5d39f6e89ed
shre2477 <shre2477@stthomas.edu>
2015-02-18 12:03:25 -0600

    My commit from local to master.
    Initial commit from my machine to master

--- HTTPServer.py
+++ HTTPServer.py
@@ -4,11 +4,11 @@
 #Adapted from http://wiki.python.org/moin/BaseHttpServer
 import time
 import BaseHTTPServer
-from os import curdir, sep
+from os import curdir, sep # sep is the most common pathname separator ('/' or ':' or '\\')
 
 #Define some variables to be used in the execution of the program
 HOST_NAME = '' # can be 'localhost' or if you change your hosts.txt file, what happens?? ;)
-PORT_NUMBER = 9001 #If you kill the server un-gracefully you may need to change this to an open socket.
+PORT_NUMBER = 9000 #If you kill the server un-gracefully you may need to change this to an open socket.
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 #MyHandler class implements standard standard HTTP menthods
@@ -18,6 +18,9 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
     def do_GET(self):
+
+        if self.path=="/":
+			self.path="/index.html"
         #Respond to a GET request.
         try:
             if self.path.endswith((".html",".htm")):
@@ -35,15 +38,28 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                 self.send_header("Content-type", "text/html")
                 self.end_headers()
                 # Send current time
-                self.wfile.write("Are the chilren all in bed because now its " + str(time.localtime()[3]) + " O'Clock!!") 
+                self.wfile.write("Are the children all in bed because now its " + str(time.localtime()[3]) + " O'Clock!!")
                 return
                 
             if self.path.endswith(".gif"):
-                # IMPLEMENT THIS
+                gif = 'thumb.gif'
+                f = open(curdir + sep + gif, 'rb')
+                self.send_response(200)
+                self.send_header("Content-type", "image/gif")
+                self.end_headers()
+                self.wfile.write(f.read())
+                f.close()
                 return
                 
             if self.path.endswith(".jpg"):
-                # IMPLEMENT THIS
+                im = 'be_linux.jpg'
+                f = open(curdir + sep + im, 'rb') #self.path has index.html
+                self.send_response(200)
+                self.send_header("Content-type", "image/jpg")
+                self.end_headers()
+                # Start sending content
+                self.wfile.write(f.read())
+                f.close()
                 return
         except IOError:
             self.send_error(404,'File not found: %self' % self.path)
@@ -53,6 +69,7 @@ if __name__ == '__main__':
     httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler) #Tell the serever what hostname & port to run on, then what handler to handle the server requests.
     print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
     try:
+        print 'Use <Ctrl-C> to stop'
         httpd.serve_forever()  #Run the server.
     except KeyboardInterrupt:
         pass
